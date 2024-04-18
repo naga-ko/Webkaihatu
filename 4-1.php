f1796a7d0e4b043b735adaa11f58f44f5c5f8eea
naga-ko <146696281+naga-ko@users.noreply.github.com>
2024-04-18 13:54:59 +0900

    Web開発基礎

---
+++ 4-1.php
@@ -0,0 +1,21 @@
+<?php
+//echo PHP_INT_MAX; //PHP_INT_MAXは定数。呼び出すときはクォート無し
+//echo  __DIR__ ;
+
+ver_dump(PHP_INT_MAX);
+//ver_dump 型の種類と内容を表示
+?>
+<?php
+ver_dump(PHP_INT_MAX*109900);
+?>
+
+<?php
+ver_dump("本日は");
+?>
+
+p66チェックテスト
+Q1
+$$$$10total_score❌
+
+Q2
+複合演算子
---
+++ text.php
@@ -0,0 +1,29 @@
+You have
+<!-- コードが短い場合は1行に <?php  ?> とかく方法もある -->
+<?php
+echo 2+3;
+?>
+cats.
+
+p49 のチェックテスト
+
+Q1
+開始タグ <?php
+終了タグ ?>
+
+Q2
+/* ... */ は、囲んだ箇所がコメントになる
+// と # は、この行がコメントになる
+
+Q3
+
+php intelephense拡張を入れると、vs codeでphpコード解析してくれる
+
+
+p53 チェックテスト
+
+Q1
+Fatal Error > Parse Error > Qarning > Notice
+
+Q2
+画面にエラーを表示させる
