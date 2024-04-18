42ba60208fe554bff106b692df39252b949ed16a
naga-ko <146696281+naga-ko@users.noreply.github.com>
2024-04-18 14:15:53 +0900

    四則演算

---
+++ four_arithmetic.php
@@ -0,0 +1,14 @@
+<?php
+$a = 8;
+$b = 3;
+
+echo $a + $b."<br>";
+echo $a - $b."<br>";
+echo $a * $b."<br>";
+echo $a / $b."<br>";
+echo $a % $b."<br>";
+
+echo"<hr>";
+
+echo 2 + 3 * 100 . "<br>";
+echo(2 + 3) * 100 . "<br>";
