dfec3a2acbaacb66b311d623bb9f0d3c304c4c65
naga-ko <146696281+naga-ko@users.noreply.github.com>
2024-04-18 16:47:11 +0900

    PHP

--- and.php
+++ and.php
@@ -1,14 +1,64 @@
+AND
 <pre>
-    <?php
-    $true = TRUE;
-    $false = FALSE;
+<?php
+$true = TRUE;
+$false = FALSE;
 
-    $a = $true && $true;
-    $b = $true && $false;
-    $c = $true && $true && $true;
+$a = $true && $true;
+$b = $true && $false;
+$c = $true && $true && $true;
 
-    $d = $true && $false && $false;
-    $e = $true && ($true && $false);
-    ver_dump($a,$b,$c,$d,$e);
-    ?>
+$d = $true && $false && $false;
+$e = $true && ($true && $false);
+var_dump($a,$b,$c,$d,$e);
+?>
+</pre>
+OR
+<pre>
+<?php
+$true = TRUE;
+$false = FALSE;
+
+$a = $true || $true;
+$b = $true || $false;
+$c = $true || $true || $true;
+$d = $true || $false || $false;
+
+$e = $true || ($true || $false);
+$f = $false || $false;
+var_dump($a,$b,$c,$d,$e,$f);
+?>
+</pre>
+NOT
+<pre>
+<?php
+$true = TRUE;
+$false = FALSE;
+
+$a = !$true;
+$b = !$false;
+$c = !$true && !$true;
+
+$d = !($true && $true);
+
+var_dump($a,$b,$c,$d);
+?>
+</pre>
+
+p74
+<pre>
+<?php
+$true = TRUE;
+$false = FALSE;
+
+$a = $true && $true;
+$b = $true && $false;
+$c = $true || $true;
+$d = $true || $false;
+$e = !$true;
+$f = !$false;
+$g = !($true && $false);
+$h = !($true && $false) && $true;
+var_dump($a,$b,$c,$d,$e,$f,$g,$h);
+?>
 </pre>
