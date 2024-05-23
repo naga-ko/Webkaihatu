228a26e56ba6e95a8962fd6c17e113d1b8f1f6d7
naga-ko <146696281+naga-ko@users.noreply.github.com>
2024-05-23 15:11:34 +0900

    更新

--- tennis/index.php
+++ tennis/index.php
@@ -7,19 +7,32 @@
   <body>
     <?php $info = file_get_contents("info.text");//ファイルからテキストデータを取ってくる?>
     <?php include('navbar.php'); ?>
-
     <main role="main" class="container" style="padding:60px 15px 0">
       <div>
         <!-- ここから「本文」-->
 
         <h1>お知らせ</h1>
-        <p><?php echo nl2br($info,false);?></p>
+        <p><?php
+    $fp = fopen("info.text","r");
+
+    $title = fgets($fp);
+    echo $title;
+        echo "<hr>";
+        // $line2 = fgets($fp);
+        // echo $line2;
+        // echo "<hr>";
+        // $line3 = fgets($fp);
+        // echo $line3;
+        // echo "<hr>";
+        while ($line = fgets($fp)){
+            echo $line . "<hr>";
+        }
+    ?></p>
 
         <!-- 本文ここまで -->
 
       </div>
     </main>
-
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
     <script>window.jQuery || document.write('<script src="/docs/4.5/assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
