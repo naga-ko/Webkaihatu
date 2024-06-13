574c37dde5564699aa12dd0cb9cf997ce53f6e8c
naga-ko <146696281+naga-ko@users.noreply.github.com>
2024-06-13 14:26:14 +0900

    更新

--- tennis/chapter4.php
+++ tennis/chapter4.php
@@ -3,6 +3,11 @@
 $firstname = '花子';
 $lastname = '山田';
 
-echo "こんにちは、" . $lastname . $firstname . "さん。";
+
+if($firstname == '' or $lastname == ''){
+    echo "名前が登録されていません";
+}else{
+    echo "こんにちは、" . $lastname . $firstname . "さん。";
+}
 
 ?>
