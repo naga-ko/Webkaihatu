780d6ceff2cef67ee0a3117d7eb4c1bf4052b1e7
naga-ko <146696281+naga-ko@users.noreply.github.com>
2024-07-18 15:08:32 +0900

    更新

---
+++ tennis/resize.php
@@ -0,0 +1,34 @@
+<?php
+$new_width = 200;
+
+list($width,$height) = getimagesize($_FILES['file']['tmp_name']);
+
+$rate = $new_width / $width;
+$new_height = $rate * $height;
+
+$canvas = imgagecreatetruecolor($new_width,$new_height);
+
+switch (exif_imagetype($_FILES['file']['tmp_name'])){
+    case IMAGETYPE_JPEG:
+        $image = imagecreatefromjpeg($_FILES['file']['tmp_name']);
+        imagecopyresampled($canvas,$image,0,0,0,0,$new_width,$new_height,$width,$height);
+        imagejpeg($canvas,'images/new_image.jpg');
+        break;
+
+    case IMAGETYPE_GIF:
+            $image = imagecreatefromgif($_FILES['file']['tmp_name']);
+            imagecopyresampled($canvas,$image,0,0,0,0,$new_width,$new_height,$width,$height);
+            imagegif($canvas,'images/new_image.gif');
+            break;
+
+        case IMAGETYPE_PNG:
+        $image = imagecreatefrompng($_FILES['file']['tmp_name']);
+        imagecopyresampled($canvas,$image,0,0,0,0,$new_width,$new_height,$width,$height);
+        imagepng($canvas,'images/new_image.png');
+        break;
+        default:
+        exit();
+}
+imagedestroy($image);
+imagedestroy($canvas);
+?>
